a16cb3eecf000681ac5dfef0a23473269f334ae0
AncientMariner <xander.lysak@gmail.com>
2024-03-06 14:09:26 +0100

    add builder and chain of responsibility for figures

--- chessboard.go
+++ chessboard.go
@@ -1,9 +1,16 @@
 package main
 
-import "strings"
+import (
+	"Chessboard_in_Go/figures"
+	"fmt"
+	"strings"
+)
 
 func main() {
 	drawEmptyBoard()
+	boardWithRookAndBishop := NewBuilder(map[string]int{"rook": 1, "bishop": 1}).withRook().withBishop().Build()
+
+	fmt.Println(boardWithRookAndBishop)
 }
 
 const defaultDimension = 8
@@ -19,4 +26,53 @@ func drawEmptyBoard() string {
 		board.WriteString("\n")
 	}
 	return board.String()
+}
+
+type Chessboard struct {
+	figureQuantityMap      map[string]int
+	currentFigureBehaviour figures.FigureBehaviour
+}
+
+type ChessBoardBuilder interface {
+	withKing() ChessBoardBuilder
+	withRook() ChessBoardBuilder
+	withBishop() ChessBoardBuilder
+	Build() *Chessboard
+}
+
+type boardBuilder struct {
+	currentFigureBehaviour figures.FigureBehaviour
+	figureQuantityMap      map[string]int
+}
+
+func (bb *boardBuilder) withRook() ChessBoardBuilder {
+	return bb.addToChain(&figures.Rook{})
+}
+
+func (bb *boardBuilder) withBishop() ChessBoardBuilder {
+	return bb.addToChain(&figures.Bishop{})
+}
+
+func (bb *boardBuilder) withKing() ChessBoardBuilder {
+	return bb.addToChain(&figures.King{})
+}
+
+func NewBuilder(figureQuantityMap map[string]int) ChessBoardBuilder {
+	return &boardBuilder{figureQuantityMap: figureQuantityMap}
+}
+
+func (bb *boardBuilder) addToChain(figure figures.FigureBehaviour) ChessBoardBuilder {
+	if bb.currentFigureBehaviour == nil {
+		bb.currentFigureBehaviour = figure
+	} else {
+		bb.currentFigureBehaviour.SetNext(figure)
+	}
+	return bb
+}
+
+func (bb *boardBuilder) Build() *Chessboard {
+	return &Chessboard{
+		figureQuantityMap:      bb.figureQuantityMap,
+		currentFigureBehaviour: bb.currentFigureBehaviour,
+	}
 }
---
+++ figures/figure.go
@@ -0,0 +1,22 @@
+package figures
+
+type Figure struct {
+	next FigureBehaviour
+}
+
+type FigureBehaviour interface {
+	SetNext(FigureBehaviour) FigureBehaviour
+	Handle(string) string
+}
+
+func (f *Figure) SetNext(next FigureBehaviour) FigureBehaviour {
+	f.next = next
+	return next
+}
+
+func (f *Figure) Handle(request string) string {
+	if f.next != nil {
+		return f.next.Handle(request)
+	}
+	return ""
+}
