4057ba025ce1a80d27879e9796f8e17ae1090965
AncientMariner <xander.lysak@gmail.com>
2024-04-29 17:42:37 +0200

    add knight initial behaviour

--- figures/knight.go
+++ figures/knight.go
@@ -2,17 +2,146 @@ package figures
 
 type Knight struct {
 	Figure
-	// add placement behaviour
 }
 
-func (knight *Knight) Handle(request string) map[string]string {
-	if request == "Hello" {
-		return nil
+func (knight *Knight) Handle(board string) map[string]string {
+
+	countOfEmptyPlaces := 0
+	for i := 0; i < len(board); i++ {
+		if board[i] == emptyField {
+			countOfEmptyPlaces++
+		}
 	}
-	if knight.next != nil {
-		knight.next.Handle(request)
+
+	boards := make(map[string]string, countOfEmptyPlaces)
+
+	for i := 0; i < len(board) && len(board) == ((defaultDimension+1)*defaultDimension); i++ {
+		if board[i] == emptyField {
+			out := []rune(board)
+
+			if !isAnotherFigurePresentBelow(out, i) && !isAnotherFigurePresentBelow(out, i) {
+				placeAttackPlaces(out, i)
+				out[i] = knight.GetName()
+
+				b := &BoardWithFigurePosition{}
+				b.Board = string(out)
+				boards[b.Hash()] = b.Board
+			}
+		}
 	}
-	return nil
+	return boards
+}
+
+func placeAttackPlaces(out []rune, position int) {
+	if position >= len(out) || position == defaultDimension || position%(defaultDimension+1) == defaultDimension {
+		return
+	}
+	currentLine := position/(defaultDimension+1) + 1
+	lineBelow := currentLine + 1
+
+	positionBelowLineLeft := position + defaultDimension + 1 - 2
+	belowLineLeft := positionBelowLineLeft/(defaultDimension+1) + 1
+	if lineBelow == belowLineLeft && positionBelowLineLeft < len(out) && out[positionBelowLineLeft] == emptyField {
+		out[positionBelowLineLeft] = attackPlace
+	}
+	positionBelowLineRight := position + defaultDimension + 1 + 2
+	belowLineRight := positionBelowLineRight/(defaultDimension+1) + 1
+	if lineBelow == belowLineRight && positionBelowLineRight < len(out) && out[positionBelowLineRight] == emptyField {
+		out[positionBelowLineRight] = attackPlace
+	}
+
+	line2Below := currentLine + 1 + 1
+
+	positionBelow2LinesLeft := position + 2*(defaultDimension+1) - 1
+	below2LineLeft := positionBelow2LinesLeft/(defaultDimension+1) + 1
+	if line2Below == below2LineLeft && positionBelow2LinesLeft < len(out) && out[positionBelow2LinesLeft] == emptyField {
+		out[positionBelow2LinesLeft] = attackPlace
+	}
+
+	positionBelow2LinesRight := position + 2*(defaultDimension+1) + 1
+	below2LineRight := positionBelow2LinesRight/(defaultDimension+1) + 1
+	if line2Below == below2LineRight && positionBelow2LinesRight < len(out) && out[positionBelow2LinesRight] == emptyField {
+		out[positionBelow2LinesRight] = attackPlace
+	}
+}
+
+func isAnotherFigurePresentBelow(out []rune, position int) bool {
+	currentLine := position/(defaultDimension+1) + 1
+	var numbersToCheck []int
+
+	positionBelowLineLeft := position + defaultDimension + 1 - 2
+	positionBelowLineRight := position + defaultDimension + 1 + 2
+
+	lineBelow := currentLine + 1
+	belowLineLeft := positionBelowLineLeft/(defaultDimension+1) + 1
+	belowLineRight := positionBelowLineRight/(defaultDimension+1) + 1
+
+	if lineBelow == belowLineLeft && positionBelowLineLeft < len(out) && out[positionBelowLineLeft] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionBelowLineLeft)
+	}
+	if lineBelow == belowLineRight && positionBelowLineRight < len(out) && out[positionBelowLineRight] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionBelowLineRight)
+	}
+
+	positionBelow2LinesLeft := position + 2*(defaultDimension+1) - 1
+	positionBelow2LinesRight := position + 2*(defaultDimension+1) + 1
+
+	below2LineLeft := positionBelow2LinesLeft/(defaultDimension+1) + 1
+	below2LineRight := positionBelow2LinesRight/(defaultDimension+1) + 1
+	line2Below := currentLine + 1 + 1
+	if line2Below == below2LineRight && positionBelow2LinesRight < len(out) && out[positionBelow2LinesRight] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionBelow2LinesRight)
+	}
+	if line2Below == below2LineLeft && positionBelow2LinesLeft < len(out) && out[positionBelow2LinesLeft] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionBelow2LinesLeft)
+	}
+
+	for _, number := range numbersToCheck {
+		if out[number] != emptyField && out[number] != attackPlace {
+			return true
+		}
+	}
+	return false
+}
+
+func isAnotherFigurePresentAbove(out []rune, position int) bool {
+
+	currentLine := position/(defaultDimension+1) + 1
+	var numbersToCheck []int
+
+	positionAboveLineLeft := position - defaultDimension - 1 - 2
+	positionAboveLineRight := position - defaultDimension - 1 + 2
+
+	lineAbove := currentLine - 1
+	aboveLineLeft := positionAboveLineLeft/(defaultDimension+1) + 1
+	aboveLineRight := positionAboveLineRight/(defaultDimension+1) + 1
+
+	if aboveLineLeft == lineAbove && positionAboveLineLeft >= 0 && out[positionAboveLineLeft] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionAboveLineLeft)
+	}
+	if aboveLineRight == lineAbove && positionAboveLineRight >= 0 && out[positionAboveLineRight] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionAboveLineRight)
+	}
+
+	positionAbove2LinesLeft := position - 2*(defaultDimension+1) - 1
+	positionAbove2LinesRight := position - 2*(defaultDimension+1) + 1
+
+	above2LineLeft := positionAbove2LinesLeft/(defaultDimension+1) + 1
+	above2LineRight := positionAbove2LinesRight/(defaultDimension+1) + 1
+	line2Above := currentLine - 1 - 1
+	if above2LineLeft == line2Above && positionAbove2LinesLeft >= 0 && out[positionAbove2LinesLeft] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionAbove2LinesLeft)
+	}
+	if above2LineRight == line2Above && positionAbove2LinesRight >= 0 && out[positionAbove2LinesRight] != '\n' {
+		numbersToCheck = append(numbersToCheck, positionAbove2LinesRight)
+	}
+
+	for _, number := range numbersToCheck {
+		if out[number] != emptyField && out[number] != attackPlace {
+			return true
+		}
+	}
+	return false
 }
 
 func (*Knight) GetName() rune {
---
+++ figures/knight_test.go
@@ -0,0 +1,199 @@
+package figures
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestKnight_GetName(t *testing.T) {
+	type fields struct {
+		Figure Figure
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   rune
+	}{
+		{"Test get name", fields{Figure{}}, 'n'},
+		{"Test get name with empty figure", fields{}, 'n'},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kn := &Knight{
+				Figure: tt.fields.Figure,
+			}
+			if got := kn.GetName(); got != tt.want {
+				t.Errorf("GetName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKnight_Handle(t *testing.T) {
+	type fields struct {
+		Figure Figure
+	}
+	type args struct {
+		board string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{"Test handle board size 8 is not possible", fields{Figure{next: nil}}, args{board: "________\n"}, 0},
+		{"Test handle empty board size 64", fields{Figure{next: nil}}, args{board: "________\n________\n________\n________\n________\n________\n________\n________\n"}, 64},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			knight := &Knight{
+				Figure: tt.fields.Figure,
+			}
+			if got := knight.Handle(tt.args.board); len(got) != tt.want {
+				t.Errorf("Handle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isAnotherFigurePresentAbove(t *testing.T) {
+	type args struct {
+		out      []rune
+		position int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Test vertically 1 above", args{
+			out:      []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 5,
+		}, false},
+		{"Test vertically 1 above", args{
+			out:      []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 10,
+		}, false},
+		{"Test vertically 1 above", args{
+			out:      []rune{'b', '_', '_', 'k', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 10,
+		}, true},
+		{"Test vertically 1 above", args{
+			out:      []rune{'b', '_', '_', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 11,
+		}, true},
+		{"Test vertically 1 above", args{
+			out:      []rune{'b', '_', '_', '_', 'k', 'q', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 16,
+		}, true},
+		{"Test vertically 1 above", args{
+			out:      []rune{'_', 'b', '_', 'b', '_', 'q', '_', '_', '\n', 'k', '_', '_', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 20,
+		}, true},
+		{"Test vertically 1 above", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', 'q', '_', '_', '\n', 'k', '_', 'r', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 18,
+		}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnotherFigurePresentAbove(tt.args.out, tt.args.position); got != tt.want {
+				t.Errorf("isAnotherFigurePresentAbove() = %v, want %v", tt.args.out, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isAnotherFigurePresentBelow(t *testing.T) {
+	type args struct {
+		out      []rune
+		position int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 5,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 10,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'b', '_', '_', 'k', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 10,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'b', '_', '_', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 11,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'b', '_', '_', '_', 'k', 'q', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 16,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', '_', 'b', '_', 'q', '_', '_', '\n', 'k', '_', '_', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 20,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', 'q', '_', '_', '\n', 'k', '_', 'r', '_', 'k', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 18,
+		}, false},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', '_', '_', '_', '\n', '_', '_', 'b', '_', '_', '_', 'b', '_', '\n', '_', '_', '_', 'b', '_', 'b', '_', '_', '\n'},
+			position: 4,
+		}, true},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', 'b', '_', '\n', '_', '_', '_', 'b', '_', 'b', '_', '_', '\n'},
+			position: 4,
+		}, true},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', 'b', '_', 'b', '_', '_', '\n'},
+			position: 4,
+		}, true},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', 'b', '_', '_', '\n'},
+			position: 4,
+		}, true},
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', 'b', 'k', 'b', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 4,
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAnotherFigurePresentBelow(tt.args.out, tt.args.position); got != tt.want {
+				t.Errorf("isAnotherFigurePresentBelow() = %v, want %v", tt.args.out, tt.want)
+			}
+		})
+	}
+}
+
+func Test_placeAttackPlacesHorizontally1(t *testing.T) {
+	type args struct {
+		out      []rune
+		position int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []rune
+	}{
+		{"Test vertically 1 below", args{
+			out:      []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n', '_', '_', '_', '_', '_', '_', '_', '_', '\n'},
+			position: 2,
+		}, []rune{'_', '_', '_', '_', '_', '_', '_', '_', '\n', 'x', '_', '_', '_', 'x', '_', '_', '_', '\n', '_', 'x', '_', 'x', '_', '_', '_', '_', '\n'}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			placeAttackPlaces(tt.args.out, tt.args.position)
+			if !reflect.DeepEqual(tt.args.out, tt.want) {
+				t.Errorf("placeAttackPlacesHorizontally1() = %v, want %v", tt.args.out, tt.want)
+			}
+		})
+	}
+}
